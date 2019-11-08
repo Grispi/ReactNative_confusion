60596a3a645958c672c84bc9a3c7a1033bf55ebf
Grisel Alpanes <griselmelina@gmail.com>
2019-11-08 10:19:55 +0000

    Components Part 2

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
 import React from "react";
 import Main from "./components/MainComponent";
+import { StyleSheet, SafeAreaView } from "react-native";
 
 export default function App() {
-  return <Main />;
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <Main />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#ddd"
+  }
+});
