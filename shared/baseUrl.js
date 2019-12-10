a7ac112e96c8a12780e0481847116cc0183d347c
Grisel Alpanes <griselmelina@gmail.com>
2019-12-10 15:41:39 +0000

    Alert

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,2 +1,3 @@
 // export const baseUrl = "http://63403cba.ngrok.io/";
+//Actualizar este link cada vez que se reinicia el puente del server :/ngrok -> copiar la url que muestra antes del puerto http:3001
 export const baseUrl = "http://c45802df.ngrok.io/";
