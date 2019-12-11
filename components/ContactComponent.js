cbc09f4f19d8b10bbe8955112ee61604898bbc52
Grisel Alpanes <griselmelina@gmail.com>
2019-12-11 11:50:37 +0000

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import { Card } from "react-native-elements";
+import * as Animatable from "react-native-animatable";
 
 function Contact() {
   return (
-    <Card title="Contact Information">
-      <Text style={{ marginBottom: 10 }}>121, Clear Water Bay Road </Text>
-      <Text style={{ marginBottom: 10 }}>Clear Water Bay, Kowloon </Text>
-      <Text style={{ marginBottom: 10 }}>HONG KONG </Text>
-      <Text style={{ marginBottom: 10 }}>Tel: +852 1234 5678 </Text>
-      <Text style={{ marginBottom: 10 }}>Fax: +852 8765 4321</Text>
-      <Text style={{ marginBottom: 10 }}>Email:confusion@food.net</Text>
-    </Card>
+    <ScrollView>
+      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+        <Card title="Contact Information">
+          <Text style={{ marginBottom: 10 }}>121, Clear Water Bay Road </Text>
+          <Text style={{ marginBottom: 10 }}>Clear Water Bay, Kowloon </Text>
+          <Text style={{ marginBottom: 10 }}>HONG KONG </Text>
+          <Text style={{ marginBottom: 10 }}>Tel: +852 1234 5678 </Text>
+          <Text style={{ marginBottom: 10 }}>Fax: +852 8765 4321</Text>
+          <Text style={{ marginBottom: 10 }}>Email:confusion@food.net</Text>
+        </Card>
+      </Animatable.View>
+    </ScrollView>
   );
 }
 
